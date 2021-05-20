105456eca5bf1ca6a09e12c66579befe55db9a85
nagaoyuriko <nagaoyuriko@nagaoyuriko.com>
2021-05-20 19:58:03 +0900

    service導入

--- app/src/main/java/jp/co/pannacotta/fiveg/DancingOldmanWidget.kt
+++ app/src/main/java/jp/co/pannacotta/fiveg/DancingOldmanWidget.kt
@@ -1,20 +1,15 @@
 package jp.co.pannacotta.fiveg
 
-import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
-import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
-import android.widget.RemoteViews
 
 /**
  * Implementation of App Widget functionality.
  */
 class DancingOldmanWidget : AppWidgetProvider() {
-    companion object {
-        const val OYAJI_CLICKED = "jp.co.pannacotta.fiveg.OYAJI_CLICKED"
-    }
+    private var intent: Intent? = null
 
     override fun onUpdate(
         context: Context,
@@ -22,10 +17,11 @@ class DancingOldmanWidget : AppWidgetProvider() {
         appWidgetIds: IntArray
     ) {
         // There may be multiple widgets active, so update all of them
-        for (appWidgetId in appWidgetIds) {
-            val widgetId = ComponentName(context, DancingOldmanWidget::class.java)
-            updateAppWidget(context, appWidgetManager, widgetId)
+        intent = Intent(context, NetworkService::class.java)
+        intent?.also {
+            it.action = "INIT"
         }
+        context.startService(intent)
     }
 
     override fun onEnabled(context: Context) {
@@ -35,39 +31,4 @@ class DancingOldmanWidget : AppWidgetProvider() {
     override fun onDisabled(context: Context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-
-    override fun onReceive(context: Context?, intent: Intent?) {
-        super.onReceive(context, intent)
-        var action = ""
-        intent?.also {
-            action = it.action.toString()
-        }
-        if(action == OYAJI_CLICKED) {
-            context?.let {
-                val appWidgetManager = AppWidgetManager.getInstance(context)
-                val views = RemoteViews(it.packageName, R.layout.dancing_oldman_wdget)
-                views.setImageViewResource(R.id.oyaji_image_view, R.drawable.fiveg)
-                val widgetId = ComponentName(it, DancingOldmanWidget::class.java)
-                appWidgetManager.updateAppWidget(widgetId, views)
-            }
-
-        }
-    }
-}
-
-internal fun updateAppWidget(
-    context: Context,
-    appWidgetManager: AppWidgetManager,
-    appWidgetId: ComponentName
-) {
-    // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.dancing_oldman_wdget)
-
-    val clickIntent = Intent(context, DancingOldmanWidget::class.java)
-    clickIntent.action = DancingOldmanWidget.OYAJI_CLICKED
-    val clickPendingIntent: PendingIntent = PendingIntent.getBroadcast(context, 0, clickIntent, 0)
-    views.setOnClickPendingIntent(R.id.transparent_button, clickPendingIntent)
-
-    // Instruct the widget manager to update the widget
-    appWidgetManager.updateAppWidget(appWidgetId, views)
 }
