b915b60f4bc28559c55592cf10488592ace9331d
hayashidamoka <sudekamo1827@gmail.com>
2021-06-05 19:42:36 +0900

    pannacota削除

--- app/src/main/java/com/teampansaru/fiveg/WalkThroughFragment.kt
+++ app/src/main/java/com/teampansaru/fiveg/WalkThroughFragment.kt
@@ -8,7 +8,6 @@ import android.widget.FrameLayout
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import jp.co.pannacotta.fiveg.R
 
 class WalkThroughFragment : Fragment() {
     var walkThroughType : WalkThroughType? = null
